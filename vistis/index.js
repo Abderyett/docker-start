c3351928d919d56df61eed613e5b232e4b4b8cf5
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2023-06-08 20:00:33 +0100

    update server js logics

--- vistis/index.js
+++ vistis/index.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const redis = require('redis');
-
 const app = express();
-const client = redis.createClient();
 
-client.set('visits', 0);
+const redisClient = redis.createClient({
+	legacyMode: true,
+	host: 'redisClient',
+	port: 6379,
+});
 
-app.get('/', (req, res) => {
-	client.get('visits', (err, visits) => {
-		res.send('Number of visits' + visits);
-		client.set('visits', parseInt(visits) + 1);
+redisClient
+	.connect()
+	.then(async (res) => {
+		console.log('connected');
+	})
+	.catch((err) => {
+		console.log('Oh no an ERROR!!' + err);
+	});
+
+app.get('/', function (req, res) {
+	redisClient.get('numVisits', function (err, numVisits) {
+		numVisitsToDisplay = parseInt(numVisits) + 1;
+		if (isNaN(numVisitsToDisplay)) {
+			numVisitsToDisplay = 1;
+		}
+		res.send('web1: Total number of visits is: ' + numVisitsToDisplay);
+		numVisits++;
+		redisClient.set('numVisits', numVisits);
 	});
 });
 
-app.listen(8081, () => {
-	console.log('Listening on port 8081... ');
+app.listen(5000, function () {
+	console.log('Web app is listening on port 5000');
 });
