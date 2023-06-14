9f4d0b441cc8c2f2d256ca975cfa24652aca852c
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2023-06-14 22:03:26 +0100

    setup worker

---
+++ fibonacci-app/worker/index.js
@@ -0,0 +1,20 @@
+const { redisHost, redisPort } = require('./keys');
+
+const redis = require('redis');
+
+const rediClient = redis.createClient({
+	host: redisHost,
+	port: redisPort,
+	retry_strategy: () => 1000,
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+	if (index < 2) return index;
+	return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+	rediClient.hset('values', message, fib(parseInt(message)));
+});
