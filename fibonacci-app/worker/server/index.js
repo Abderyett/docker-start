83fa924af55580fe85c0b2e1951c59a6a40b2fac
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2023-06-15 07:59:25 +0100

    connecting server to redis and setp routes to retrive data from PG en redis

--- fibonacci-app/worker/server/index.js
+++ fibonacci-app/worker/server/index.js
@@ -34,3 +34,35 @@ pgClient.on('error', (err) => console.log('Lost PG Connection...'));
 pgClient
 	.query('CREATE TABLE IF NOT EXISTS values (number INT)')
 	.catch((err) => console.log('Tbale has not been created ...', err));
+
+// Redis Client setup
+
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+	hsot: redisHost,
+	port: redisPort,
+	retry_strategy: () => 1000,
+});
+
+const redisPublisher = redisClient.duplicate();
+
+//Express routes
+
+app.get('/', (req, res) => {
+	res.send('working');
+});
+
+app.get('/values/all', async (req, res) => {
+	const values = await pgClient.query('SELECT * FROM values');
+
+	res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+	redisClient.hgetall('values', (err, values) => {
+		res.send(values);
+	});
+});
+
+app.listen(8080, () => console.log('Listening to port 8080 ...'));
