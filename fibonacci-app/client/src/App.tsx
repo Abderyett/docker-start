a736267fed885c5b7a93b8230eac437d141883bc
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2023-06-16 12:00:57 +0100

    finalize frontend part

--- fibonacci-app/client/src/App.tsx
+++ fibonacci-app/client/src/App.tsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Link, Routes } from 'react-router-dom';
+import './App.css';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div>
+				<Link to='/'>Home</Link>
+				<Link to='/otherpage'>Other page</Link>
+			</div>
+			<Routes>
+				<Route path='/' element={<Fib />} />
+				<Route path='/otherpage' element={<OtherPage />} />
+			</Routes>
+		</>
+	);
 }
 
-export default App
+export default App;
--- fibonacci-app/client/src/Fib.tsx
+++ fibonacci-app/client/src/Fib.tsx
@@ -1,4 +1,4 @@
-import { useState, useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios, { CanceledError } from 'axios';
 interface Data {
 	seenIndex: number[];
@@ -19,8 +19,22 @@ const Fib = () => {
 			.get('/api/values/current', {
 				signal: controller.signal,
 			})
+			.then((res) => {
+				setFormData({ ...formData, value: res.data });
+			})
+			.catch((err) => {
+				if (err instanceof CanceledError) return;
+			});
+
+		axios
+			.get('/api/values/all', {
+				signal: controller.signal,
+			})
 			.then((result) => {
-				setFormData({ ...formData, values: result.data });
+				setFormData({
+					...formData,
+					seenIndex: [...formData.seenIndex, result.data],
+				});
 			})
 			.catch((err) => {
 				if (err instanceof CanceledError) return;
@@ -29,7 +43,42 @@ const Fib = () => {
 		return () => controller.abort();
 	}, []);
 
-	return <div>Fib</div>;
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setFormData((prevState) => ({
+			...prevState,
+			index: event.target.value,
+		}));
+	};
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		await axios.post('/api/value', { index: formData.index });
+		setFormData((prevState) => ({
+			...prevState,
+			index: '',
+		}));
+	};
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label>Enter your index:</label>
+				<input type='number' value={formData.index} onChange={handleChange} />
+
+				<button>submit</button>
+			</form>
+			<h3>Indecies I have seen:</h3>
+
+			{formData.seenIndex &&
+				formData.seenIndex.map((number) => number).join(' ,')}
+
+			<h3>Calculated Values</h3>
+
+			{Object.keys(formData.value).map((key) => (
+				<div key={key}>For Index {key} I claculated</div>
+			))}
+		</div>
+	);
 };
 
 export default Fib;
